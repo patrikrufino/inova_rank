be9e7410373302802486fb5a00ed562987804f0c
Patrik Rufino <patrik.rufino@gmail.com>
2025-03-09 19:55:39 -0300

    ✨ (feat): Refatora a página de login com novo design e validação de formulário

--- inova_rank_ui/pages/auth/login.vue
+++ inova_rank_ui/pages/auth/login.vue
@@ -1,51 +1,86 @@
 <template>
-    <UContainer class="scroll-smooth">
-            <UCard class="my-5 hover:cursor-pointer hover:shadow-lg hover:shadow-primary/30 transition-shadow duration-1000 delay-100 max-w-lg mx-auto">
-                <h2 class="text-2xl font-bold mb-6 text-center">Login</h2>
-                <form @submit.prevent="login">
-                    <div class="mb-4">
-                        <label class="block text-sm font-bold mb-2" for="email">Email</label>
-                        <input v-model="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Digite seu email" required>
+    <UContainer class="scroll-smooth flex flex-col justify-center items-center min-h-screen">
+        <img src="/login_hero.svg" alt="Homem sentado realizando login" class="w-48 mb-5"/>
+        <UCard
+            class="w-full md:max-w-1/2 my-5 hover:cursor-pointer hover:shadow-lg hover:shadow-primary/30 transition-shadow duration-1000 delay-100 max-w-lg mx-auto">
+
+            <template #header>
+                <h1 class="text-2xl font-bold text-center">Login</h1>
+            </template>
+            <UForm :schema="schema" :state="state" @submit="onSubmit" class="space-y-4">
+                <UFormGroup label="Email" name="email">
+                    <UInput v-model="state.email" placeholder="Digite seu email" size="lg" />
+                </UFormGroup>
+                <UFormGroup label="Senha" name="password">
+                    <div class="relative">
+                        <UInput :type="passwordVisible ? 'text' : 'password'" v-model="state.password"
+                            placeholder="Digite sua senha" size="lg" />
+                        <button type="button" @click="togglePasswordVisibility"
+                            :class="passwordVisible ? 'text-primary' : ''"
+                            class="absolute inset-y-0 right-0 pr-3 flex items-center">
+                            <UIcon :name="passwordVisible ? 'i-mdi-eye' : 'i-mdi-eye-off'" class="w-6 h-6" />
+                        </button>
                     </div>
-                    <div class="mb-6">
-                        <label class="block text-sm font-bold mb-2" for="password">Senha</label>
-                        <input v-model="password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="Digite sua senha" placeholder="Password" required>
-                    </div>
-                    <div class="flex items-center justify-between">
-                        <UButton block class="bg-primary hover:bg-emerald-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" type="submit">
-                            Login
-                        </UButton>
-                    </div>
-                </form>
+                </UFormGroup>
+                <UButton block
+                    class="bg-primary hover:bg-emerald-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+                    type="submit">
+                    Login
+                </UButton>
                 <div class="text-center flex justify-center items-center mt-4">
                     <p class="mr-2 text-sm">Novo no InovaRank?</p>
-                    <NuxtLink to="/auth/register" class="text-sm font-semibold text-primary hover:text-emerald-600 underline">Crie sua conta aqui.</NuxtLink>
+                    <NuxtLink to="/auth/register"
+                        class="text-sm font-semibold text-primary hover:text-emerald-600 underline">Crie
+                        sua conta aqui.</NuxtLink>
                 </div>
-                <div class="text-center flex justify-center items-center mt-4">
-                    <p class="mr-2 text-sm">Esqueceu sua senha?</p>
-                    <NuxtLink to="/auth/forgot-password" class="underline font-semibold text-sm text-primary hover:text-emerald-600">Clique aqui.</NuxtLink>
-                </div>
-            </Ucard>
+            </UForm>
+            <div class="text-center flex justify-center items-center mt-4">
+                <p class="mr-2 text-sm">Esqueceu sua senha?</p>
+                <NuxtLink to="/auth/forgot-password"
+                    class="underline font-semibold text-sm text-primary hover:text-emerald-600">
+                    Clique aqui.</NuxtLink>
+            </div>
+        </UCard>
     </UContainer>
-
 </template>
 
-<script>
+<script lang="ts">
+import * as v from 'valibot';
+import type { FormSubmitEvent } from '#ui/types';
+import { UForm } from '#build/components';
+import { reactive, ref } from 'vue';
+
 export default {
-    data() {
-        return {
+    setup() {
+        const schema = v.object({
+            email: v.pipe(v.string(), v.email('Invalid email')),
+            password: v.pipe(v.string(), v.minLength(8, 'Must be at least 8 characters'))
+        });
+
+        type Schema = v.InferOutput<typeof schema>;
+
+        const state = reactive({
             email: '',
             password: ''
-        };
-    },
-    methods: {
-        register() {
-            // Handle registration logic here
-            console.log('Registering:', this.name, this.email, this.password);
-        }
-    }
-};
-</script>
+        });
 
-<style scoped>
-</style>
+        const passwordVisible = ref(false);
+
+        const togglePasswordVisibility = () => {
+            passwordVisible.value = !passwordVisible.value;
+        };
+
+        async function onSubmit(event: FormSubmitEvent<Schema>) {
+            console.log(event.data);
+        }
+
+        return {
+            schema,
+            state,
+            onSubmit,
+            passwordVisible,
+            togglePasswordVisibility
+        };
+    }
+}
+</script>
